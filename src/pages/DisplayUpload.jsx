e9e2806255c34cd826c6041e3c4af9e5c5eb7387
sammy-code98 <sammysmart98@yahoo.com>
2022-05-03 10:33:54 +0100

    clean up

--- src/pages/DisplayUpload.jsx
+++ src/pages/DisplayUpload.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 export default function DisplayUpload() {
   // create state and set to empty array
   const [images, setImages] = useState([]);
@@ -7,41 +7,50 @@ export default function DisplayUpload() {
   const [imageUrl, setImageUrl] = useState([]);
 
   useEffect(() => {
-    if (images.length < 1) return;
+    // image.length < 1
+    if (images.length !== 0) return;
 
-    const newImageUrl = [];
-    images.forEach((image) => newImageUrl.push(URL.createObjectURL(image)));
-    setImageUrl(newImageUrl);
+    const newImageUrls = [];
+    images.forEach((image) => newImageUrls.push(URL.createObjectURL(image[0])));
+    setImageUrl(newImageUrls);
+
   }, [images]);
+  console.log("newarr:", images);
 
 
   const onChangeHandler = (e) => {
-    setImages([e.target.files]);
+    setImages([...e.target.files]);
   };
   return (
-      <>
-        <Link
-          to="/"
-          className="bg-blue-700 text-white md:px-12 py-2 rounded-md font-mono font-bold float-right mt-4 mx-4"
-        >
-          Go Home{" "}
-        </Link>
-       <div className="container flex items-center justify-center h-screen">
-      <input
-        className="border border-blue-700 mb-12  px-4 py-2  font-mono text-gray-500 rounded-md md:px-12"
-        type="file"
-        multiple
-        accept="image/*"
-        onChange={onChangeHandler}
-      />
+    <>
+      <Link
+        to="/"
+        className="bg-blue-700 text-white px-2 md:px-12 py-2 rounded-md font-mono font-bold float-right mt-4 mx-4"
+      >
+        Go Home{" "}
+      </Link>
+      <div className="h-screen">
+      <div className="container flex items-center justify-center ">
+        <input
+          className="border border-blue-700 mb-12 mt-24  px-4 py-2  font-mono text-gray-500 rounded-md md:px-12"
+          type="file"
+          multiple
+          accept="image/*"
+          onChange={onChangeHandler}
+        />
 
-      <div>
-        {imageUrl.map((imageSrc) => (
+        
+      </div>
+      <div className="border border-blue-700  mb-12 mx-12 py-2">
+          {/* {images.map((image) => <p>{image.name}</p>)} */}
+          {imageUrl.map((imageSrc) => (
           <img src={imageSrc}  alt="dummy"/>
         ))}
+        
+
+        </div>
       </div>
-    </div>
-      </>
-   
+      
+    </>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,10 @@ export default function LandingPage() {
   return (
     <div className="container flex items-center justify-center h-screen">
       <div className="flex items-center gap-12">
-        <Link to="/normal-upload" className="bg-blue-700 text-white md:px-12 py-2 rounded-md font-mono font-bold">
+        <Link to="/normal-upload" className="bg-blue-700 text-white px-2 md:px-12 py-2 rounded-md font-mono font-bold">
           Normal Upload
         </Link>
-        <Link to="display-upload" className="bg-blue-700 text-white md:px-12 py-2 rounded-md font-mono font-bold">
+        <Link to="display-upload" className="bg-blue-700 text-white px-2 md:px-12 py-2 rounded-md font-mono font-bold">
           Display Upload
         </Link>
       </div>
